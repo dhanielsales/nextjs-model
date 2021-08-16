4fcb65fe04ebdd3680a0d9dab000d2e239253a05
Dhanielr <dhanielr@hotmail.com>
2021-08-16 00:06:10 -0300

    fix page home

--- src/frontend/modules/home/index.tsx
+++ src/frontend/modules/home/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Home: React.FC = () => {
   return (
     <Box d="flex" alignItems="center" justifyContent="center" height="100%">
-      <Text fontSize="xl" marginBottom="10px" fontWeight="bold">
+      <Text fontSize="xl" color="blue" fontWeight="bold">
         HOME
       </Text>
     </Box>
