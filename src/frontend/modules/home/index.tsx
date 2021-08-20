3ecaf2370d5dad90b8096191e4f9abae2054fe1b
Dhanielr <dhanielr@hotmail.com>
2021-08-20 08:33:54 -0300

    change color

--- src/frontend/modules/home/index.tsx
+++ src/frontend/modules/home/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Home: React.FC = () => {
   return (
     <Box d="flex" alignItems="center" justifyContent="center" height="100%">
-      <Text fontSize="xl" color="blue" fontWeight="bold">
+      <Text fontSize="xl" color="#202080" fontWeight="bold">
         HOME
       </Text>
     </Box>
